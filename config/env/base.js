5017c02d6fbde74d6e1f9d9680c99f2ed0ffb70c
Julio Canares <juliocanares@gmail.com>
2014-10-15 01:03:46 -0700

    add paths to app structure

--- config/env/base.js
+++ config/env/base.js
@@ -1,1 +1,13 @@
-base.js
+var path = require('path'),
+    rootPath = path.normalize(__dirname + '/../..');
+
+module.exports = {
+    root: rootPath,
+    port: process.env.PORT || 3000,
+    models: rootPath + '/app/models',
+    views: rootPath + '/app/views',
+    controllers: rootPath + '/app/controllers',
+    middlewares: rootPath + '/config/middlewares',
+    routes: rootPath + '/app/routes',
+    public: rootPath + '/public'
+};
