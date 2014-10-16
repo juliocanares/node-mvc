3aa8accf1e6bd6a6fb3dfd79f389153d8eddcc11
Julio Canares <juliocanares@gmail.com>
2014-10-16 02:04:08 -0700

    add basic placement of routes in the config directory

---
+++ config/routes.js
@@ -0,0 +1,15 @@
+/**
+ * Load routers
+ * ====================================================
+ */
+var pagesRouter = require(global.cf.routes + '/pages');
+var userRouter = require(global.cf.routes + '/user');
+
+/**
+ * Setup routers
+ * ====================================================
+ */
+exports.init = function (app) {
+    app.use('/', pagesRouter);
+    app.use('/user', userRouter);
+};
