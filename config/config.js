f2466902b6b27b07a8cd22419e4743fb3dc66154
Julio Canares <juliocanares@gmail.com>
2014-10-16 01:35:41 -0700

    change all.js by base.js

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 var _ = require('lodash');
 
 module.exports = _.assign(
-    require(__dirname + '/../config/env/all.js'),
+    require(__dirname + '/../config/env/base.js'),
     require(__dirname + '/../config/env/' + process.env.NODE_ENV + '.js') || {}
 );
