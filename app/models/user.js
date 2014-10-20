101d88cc88b0a80c4534862f57cb18c09aeb6578
Julio Canares <juliocanares@gmail.com>
2014-10-20 02:56:58 -0700

    add photo row

--- app/models/user.js
+++ app/models/user.js
@@ -2,7 +2,8 @@ module.exports = function (sequelize, DataTypes) {
     var User = sequelize.define('User', {
 	      username: DataTypes.STRING,
 	      firstname: DataTypes.STRING,
-	      lastname: DataTypes.STRING
+	      lastname: DataTypes.STRING,
+	      photo: DataTypes.STRING
 	    }
     );
     return User;
