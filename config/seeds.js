96875f3557e91a60b825f5f9710c855b883bbc9f
Julio Canares <juliocanares@gmail.com>
2014-10-20 02:43:48 -0700

    add user seed

---
+++ config/seeds.js
@@ -0,0 +1,20 @@
+var Chance = require('chance');
+var chance = new Chance();
+
+exports.start = function () {
+    var seeds = [seedUser(20)];
+    return global.db.Sequelize.Promise.all(seeds);
+};
+
+var seedUser = function (numOfUsers) {
+    var i, promises = [];
+    for (i = 0; i < numOfUsers; i++) {
+        promises.push(global.db.User.create({
+            username: chance.hashtag().replace('#', ''),
+            firstname: chance.name(),
+            lastname: chance.last()
+        }));
+    }
+
+    return global.db.Sequelize.Promise.all(promises);
+};
