176d89f520151e5acebf037ebc2e768ff37c8a46
Julio Canares <juliocanares@gmail.com>
2014-10-20 02:51:33 -0700

    add profile router

--- app/routes/user.js
+++ app/routes/user.js
@@ -4,5 +4,6 @@ var router = express.Router();
 var controller = require(global.cf.controllers + "/user");
 
 router.get('/', controller.index);
+router.get('/:id', controller.profile);
 
 module.exports = router;
