f49246307ceb2de5bf85edd8bb03a1742cb6b224
Julio Canares <juliocanares@gmail.com>
2014-10-20 02:46:15 -0700

    usng orm for gettings all users

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -1,5 +1,8 @@
 var basePath = 'user/';
 
 exports.index = function (req, res) {
+	global.db.User.findAll().then(function(data){
+		return res.json({data: data})
     return res.render(basePath + 'index');
+	});
 };
