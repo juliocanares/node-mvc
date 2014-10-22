4d84c77a13106d904f0aebdeb28f6ae61737e0eb
Julio Canares <juliocanares@gmail.com>
2014-10-22 03:12:46 -0700

    add about and contact actions

--- app/controllers/pages.js
+++ app/controllers/pages.js
@@ -3,3 +3,11 @@ var basePath = 'pages/';
 exports.index = function (req, res) {
     return res.render(basePath + 'index');
 };
+
+exports.about = function (req, res) {
+    return res.render(basePath + 'about');
+};
+
+exports.contact = function (req, res) {
+    return res.render(basePath + 'contact');
+};
