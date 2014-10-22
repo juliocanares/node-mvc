57652c6d917a6e7c97a42ecfbafaecd65b4a4039
Julio Canares <juliocanares@gmail.com>
2014-10-22 03:12:53 -0700

    add about and contact router

--- app/routes/pages.js
+++ app/routes/pages.js
@@ -4,5 +4,7 @@ var router = express.Router();
 var controller = require(global.cf.controllers + "/pages");
 
 router.get('/', controller.index);
+router.get('/about', controller.about);
+router.get('/contact', controller.contact);
 
 module.exports = router;
