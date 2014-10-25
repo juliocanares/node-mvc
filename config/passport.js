27894330ccd999df477f1cfa6359fcb353e6e1ff
Julio Canares <juliocanares@gmail.com>
2014-10-25 03:34:35 -0700

    export local strategy

--- config/passport.js
+++ config/passport.js
@@ -17,3 +17,22 @@ passport.deserializeUser(function (id, done) {
         done(null, user);
     })
 });
+
+/**
+ * Authentication by Local strategy
+ */
+var localStrategyHandler = function (email, password, done) {
+    global.db.User.find({where: {email: email}}).then(function (user) {
+        if (!user)
+            done(null, false, 'Unknown user');
+        else if (!user.authenticate(password))
+            done(null, false, 'Invalid password');
+        else
+            done(null, user);
+    });
+};
+
+var localStrategyData = {usernameField: 'email', passwordField: 'password'};
+passport.use(new LocalStrategy(localStrategyData, localStrategyHandler));
+
+module.exports = passport;
