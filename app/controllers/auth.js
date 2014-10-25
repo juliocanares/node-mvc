5bada3f52114cd79d1adbc242ab8a261122ceedb
Julio Canares <juliocanares@gmail.com>
2014-10-25 03:40:13 -0700

    add user created action

--- app/controllers/auth.js
+++ app/controllers/auth.js
@@ -8,6 +8,17 @@ exports.signupPage = function (req, res) {
 };
 
 /**
+ * User signup
+ */
+exports.signup = function (req, res) {
+  var options = {password: req.body.password};
+  global.db.User.create(req.body, options).then(function (user) {
+      loginUser(req, res, user);
+  });
+};
+
+
+/**
  * Show the view page for login
  */
 exports.loginPage = function (req, res) {
