7df0e107f0aa5bdc8c50c90242bad0b51eee722a
Julio Canares <juliocanares@gmail.com>
2014-10-26 03:40:56 -0700

    add login and signup post routers

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -5,5 +5,7 @@ var controller = require(global.cf.controllers + "/auth");
 
 router.get('/login', controller.loginPage);
 router.get('/signup', controller.signupPage);
+router.post('/login', controller.login);
+router.post('/signup', controller.signup);
 
 module.exports = router;
