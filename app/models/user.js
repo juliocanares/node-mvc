c0b53ddee52fda9bda86915b0b305f49853f6301
Julio Canares <juliocanares@gmail.com>
2014-10-26 03:45:37 -0700

    add email row in model user

--- app/models/user.js
+++ app/models/user.js
@@ -1,6 +1,7 @@
 module.exports = function (sequelize, DataTypes) {
     var User = sequelize.define('User', {
 	      username: DataTypes.STRING,
+	      email: DataTypes.STRING,
 	      firstname: DataTypes.STRING,
 	      lastname: DataTypes.STRING,
 	      photo: DataTypes.STRING
