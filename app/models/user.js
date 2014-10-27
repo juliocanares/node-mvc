2cb5512d4243a031af4eebb14e4a0c4bf061bbfb
Julio Canares <juliocanares@gmail.com>
2014-10-27 03:56:37 -0700

    remove photo row and add afterCreate hook

--- app/models/user.js
+++ app/models/user.js
@@ -7,26 +7,35 @@ module.exports = function (sequelize, DataTypes) {
 	      email: DataTypes.STRING,
 	      firstname: DataTypes.STRING,
 	      lastname: DataTypes.STRING,
-	      photo: DataTypes.STRING
+	      fullname: DataTypes.STRING
 	    },
 	    {
 	    	instanceMethods: {
           makeSalt: function () {
-              return crypto.randomBytes(16).toString('base64');
+            return crypto.randomBytes(16).toString('base64');
           },
           authenticate: function (password) {
-              return this.encryptPassword(password, this.salt) === this.hashedPassword;
+            return this.encryptPassword(password, this.salt) === this.hashedPassword;
           },
           encryptPassword: function (password, salt) {
-              if (!password || !salt) return '';
-              salt = new Buffer(salt, 'base64');
-              return crypto.pbkdf2Sync(password, salt, 10000, 64).toString('base64');
+            if (!password || !salt) return '';
+            salt = new Buffer(salt, 'base64');
+            return crypto.pbkdf2Sync(password, salt, 10000, 64).toString('base64');
           },
           makeTokenResetPassword: function () {
-              this.tokenResetPassword = uuid.v4();
-              this.tokenResetPasswordExpires = moment().add(1, 'hour');
-              return this.save();
+            this.tokenResetPassword = uuid.v4();
+            this.tokenResetPasswordExpires = moment().add(1, 'hour');
+            return this.save();
           }
+        },
+        hooks: {
+          afterCreate: function (user, options) {
+            options.password = options.password || '123';
+            user.salt = user.makeSalt();
+            user.hashedPassword = user.encryptPassword(options.password, user.salt);
+            user.tokenVerifyEmail = uuid.v4();
+            user.fullname = user.firstname + ' ' + user.lastname;
+            return user.save();
         }
 	    }
     );
